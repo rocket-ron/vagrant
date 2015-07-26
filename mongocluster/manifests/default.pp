c48e4faf79c2f24c8661312a47c55f8ca7fd2273
Ron Cordell <ron.cordell@berkeley.edu>
2015-07-26 15:54:08 -0700

    adding a new mongo replica set setup

---
+++ mongocluster/manifests/default.pp
@@ -0,0 +1,111 @@
+Exec { path => [ "/bin/", "/sbin", "/usr/bin", "/usr/sbin" ] }
+
+class system-update {
+  exec { 'yum update':
+    command => 'yum update -y',
+  }
+}
+
+class dev-tools {
+  exec { 'dev tools':
+    command => 'yum groupinstall \"Development Tools\" -y',
+    require => Class["system-update"],
+  }
+}
+
+class install-git {
+  exec { 'install git':
+    command => 'yum install -y git',
+    require => Class['dev-tools'],
+  }
+}
+
+class epel-release {
+  exec { 'epel release':
+    command => "yum install -y epel-release",
+  }
+}
+
+class python-pip {
+  exec { 'install pip':
+    command => 'yum install -y python-pip',
+    require => Class["dev-tools", "epel-release"],
+  }
+}
+
+class upgrade-pip {
+  exec{ 'upgrade pip':
+    command => "pip install 'pip>1.5' --upgrade",
+    require => Class["python-pip"],
+    }
+}
+
+class python-modules {
+  exec { 'tweepy':
+    command => 'pip install tweepy',
+    require => Class["upgrade-pip"],
+    }
+  exec { 'pymongo':
+    command => 'pip install pymongo',
+    require => Class["upgrade-pip"],
+  }
+}
+
+class {'::mongodb::globals':
+  manage_package_repo => true,
+}->
+class {'::mongodb::server':
+  verbose => true,
+  bind_ip => ['0.0.0.0'],
+  replset => 'rs1',
+  require => Class["system-update"],
+}->
+class {'::mongodb::client':}
+
+mongodb_replset { rs1:
+  ensure    => present,
+  members   => ['172.31.16.10:27017','172.31.16.11:27017'],
+}
+
+class user-creation {
+  user { "rcordell":
+    name 	=> 'rcordell',
+    ensure 	=> 'present',
+    shell	=> '/bin/bash',
+    password	=> '685addb7fc7d06ec07b91a611ff351e5',
+    managehome => true,
+  }
+}
+
+class my_fw::pre {
+  Firewall {
+    require => undef,
+  }
+
+  firewall { '100 accept traffic on mongoDB port 27017':
+    port    => 27017,
+    proto   => 'tcp',
+    action  => 'accept',
+  }
+}
+
+resources { 'firewall':
+  purge => false,
+}
+
+Firewall {
+  require => Class['my_fw::pre'],
+}
+
+
+include my_fw::pre
+
+include install-git
+include epel-release
+include dev-tools
+include python-pip
+include system-update
+include python-modules
+include upgrade-pip
+include stdlib
+include user-creation
