c315b8900d8fe665d5b9a16d405d96545eac8aa4
Ron Cordell <ron.cordell@berkeley.edu>
2015-09-15 00:01:56 -0700

    rabbitmq puppet module setup and testing

---
+++ rmqcluster/puppet/manifests/site.pp
@@ -0,0 +1,53 @@
+node 'node1.rmqnode.localdomain' {
+  include ::role::rmqnode::server::vagrant
+}
+
+class role {
+  include ::profile::base 
+
+}
+
+class role::rmqnode::server inherits role {
+  include ::profile::rmqnode::rabbitmq
+}
+
+class role::rmqnode::server::vagrant inherits role::rmqnode::server {
+#  include ::profile::rmqnode::networking::vagrant
+  include ::profile::rmqnode::rabbitmq::vagrant
+}
+
+# Profile definitions
+class profile::base {
+  include stdlib
+  anchor{'begin':}
+  anchor{'end':}
+  package {[
+    'git']:
+      ensure => installed, 
+  }
+  class {'::epel':}
+  user { "rcordell":
+    name  => 'rcordell',
+    ensure  => 'present',
+    shell => '/bin/bash',
+    password  => '685addb7fc7d06ec07b91a611ff351e5',
+    managehome => true,
+  }
+}
+
+class profile::rmqnode::rabbitmq inherits profile::base {
+  class {'::erlang': }
+}
+
+class profile::rmqnode::rabbitmq::vagrant inherits profile::rmqnode::rabbitmq {
+  class {'::rabbitmq':
+    port      => '15672',
+    default_user  => 'admin',
+    default_pass  => 'Passw0rd',
+    package_source => 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.5.4/rabbitmq-server-3.5.4-1.noarch.rpm',
+    package_provider => rpm,
+    package_ensure => installed,
+    service_ensure => running,
+  }
+}
+
