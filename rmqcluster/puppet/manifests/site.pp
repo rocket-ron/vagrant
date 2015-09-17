bea2a89fc93c1f23e3196fb99b801274e233e885
Ron Cordell <ron.cordell@berkeley.edu>
2015-09-16 23:31:01 -0700

    single rmq node up and running on vmware fusion

--- rmqcluster/puppet/manifests/site.pp
+++ rmqcluster/puppet/manifests/site.pp
@@ -1,10 +1,13 @@
-node 'node1.rmqnode.localdomain' {
+node 'rmq01.rmqnode.localdomain' {
   include ::role::rmqnode::server::vagrant
 }
 
 class role {
   include ::profile::base 
-
+  package { "erlang": 
+    ensure => installed,
+    require => Yumrepo["erlang-solutions"]
+  }
 }
 
 class role::rmqnode::server inherits role {
@@ -21,11 +24,18 @@ class profile::base {
   include stdlib
   anchor{'begin':}
   anchor{'end':}
+  class {'::epel':}
+  yumrepo { "erlang-solutions":
+    baseurl    => 'http://packages.erlang-solutions.com/rpm/centos/$releasever/$basearch',
+    descr      => 'The erlang_solutions repository',
+    enabled    => '1',
+    gpgcheck   => '1',
+    gpgkey     => 'http://packages.erlang-solutions.com/rpm/erlang_solutions.asc'
+  }
   package {[
     'git']:
       ensure => installed, 
   }
-  class {'::epel':}
   user { "rcordell":
     name  => 'rcordell',
     ensure  => 'present',
@@ -36,18 +46,18 @@ class profile::base {
 }
 
 class profile::rmqnode::rabbitmq inherits profile::base {
-  class {'::erlang': }
 }
 
 class profile::rmqnode::rabbitmq::vagrant inherits profile::rmqnode::rabbitmq {
   class {'::rabbitmq':
-    port      => '15672',
+    port      => '5672',
     default_user  => 'admin',
-    default_pass  => 'Passw0rd',
+    default_pass  => 'admin',
     package_source => 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.5.4/rabbitmq-server-3.5.4-1.noarch.rpm',
     package_provider => rpm,
     package_ensure => installed,
     service_ensure => running,
+    require => Package['erlang']
   }
 }
 
